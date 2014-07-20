fb9f3b03dbf29400b562aa54a35b306ce393c638
srsajid <sr667722>
2014-07-20 20:35:41 +0600

    adminssion update

--- app/views/Admission/tableView.php
+++ app/views/Admission/tableView.php
@@ -1,7 +1,7 @@
 <div class="head">
     <nav class="navbar navbar-default" role="navigation">
         <div class="navbar-header">
-            <a class="navbar-brand">Title</a>
+            <a class="navbar-brand">dfd</a>
         </div>
         <div>
             <div class="navbar-form navbar-right" role="search">
