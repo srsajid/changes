a82f70ed5ddead0e479319093c5313aaf67f65a4
rashad <rashad_rat@yahoo.com>
2014-08-29 12:22:06 +0600

    probWith database. Only Income and expense

---
+++ app/database/migrations/2014_08_29_061221_create_income_entry_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateIncomeEntryTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+        Schema::create("income_entries", function(Blueprint $table) {
+            $table->increments("id");
+            $table->double("amount");
+            $table->string("comment")->nullable();
+            $table->foreign("income_type_id")->references("id")->on("income_types");
+            $table->timestamps();
+        });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+        Schema::drop("income_entries");
+	}
+
+}
---
+++ app/database/migrations/2014_08_29_061845_create_expense_entry_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateExpenseEntryTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+        Schema::create("expense_entries", function(Blueprint $table) {
+            $table->increments("id");
+            $table->double("amount");
+            $table->string("comment")->nullable();
+            $table->foreign("expense_type_id")->references("id")->on("expense_types");
+            $table->timestamps();
+        });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+        Schema::drop("expense_entries");
+	}
+
+}
